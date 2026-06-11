93c572481d5b8f098013c2e330e1d25735231662
亚衣琴 <Saoutax@gmail.com>
2026-06-12 06:35:05 +0800

    chore: top banner

--- scripts/config.ts
+++ scripts/config.ts
@@ -11,11 +11,11 @@ const userAgent = env['USERAGENT']!;
 const apiUrl = 'https://wiki.biligame.com/pod/api.php';
 
 const banner = `/**
- * ------------------------------------------------------
+ * -------------------------------------------------------------------------------------
  * !!!                      请勿手动修改本页面内容，否则你的编辑将被覆盖                 !!!
  * !!!       DON'T MODIFY THIS PAGE MANUALLY, YOUR CHANGES WILL BE OVERWRITTEN       !!!
  * !!!  Repository URL: https://github.com/Saoutax/The-Piper-of-Dawn-InterfaceCodes  !!!
- * ------------------------------------------------------
+ * -------------------------------------------------------------------------------------
  */`; // 脚本上方提示，需为 JavaScript 与 CSS 均接受的注释格式
 
 export { SESSDATA, userAgent, apiUrl, banner };
