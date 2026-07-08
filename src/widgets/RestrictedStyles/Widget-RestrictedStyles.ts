552eb039868f46971da1902c1f674353d9ba2285
亚衣琴 <Saoutax@gmail.com>
2026-07-08 09:37:02 +0800

    fix: rlq

--- src/widgets/RestrictedStyles/Widget-RestrictedStyles.ts
+++ src/widgets/RestrictedStyles/Widget-RestrictedStyles.ts
@@ -1,10 +1,13 @@
-(() => {
-    const { wgCanonicalSpecialPageName } = mw.config.get();
-    const allowList = ['Preferences', 'Gadgets', 'Userlogin'];
-    if (wgCanonicalSpecialPageName && allowList.includes(wgCanonicalSpecialPageName)) {
-        const link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = 'https://wiki.biligame.com/pod/load.php?lang=zh-cn&modules=site.styles&only=styles&skin=vector';
-        document.head.append(link);
-    }
-})();
+(window.RLQ ||= []).push([
+    'mediawiki.base',
+    () => {
+        const { wgCanonicalSpecialPageName } = mw.config.get();
+        const allowList = ['Preferences', 'Gadgets', 'Userlogin'];
+        if (wgCanonicalSpecialPageName && allowList.includes(wgCanonicalSpecialPageName)) {
+            const link = document.createElement('link');
+            link.rel = 'stylesheet';
+            link.href = 'https://wiki.biligame.com/pod/load.php?lang=zh-cn&modules=site.styles&only=styles&skin=vector';
+            document.head.append(link);
+        }
+    },
+]);
