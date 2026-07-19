9adebc6a76268ba9d3f2e13b6c982d884dbc2456
亚衣琴 <Saoutax@gmail.com>
2026-07-19 17:23:42 +0800

    feat: statistics

---
+++ src/widgets/Statistics/Widget-Statistics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement('script');
+    hm.src = 'https://hm.baidu.com/hm.js?a1a2bd4be0b92a264572a688c65e0ff0';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(hm, s);
+})();
